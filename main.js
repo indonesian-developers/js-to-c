3242966e38ee8e9eb6bfd187528e6611370cc886
Dank-Memer-Help <86004860+Dank-Memer-Help@users.noreply.github.com>
2022-09-18 12:10:00 +0700

    Main File
    The file that exports the module

---
+++ main.js
@@ -0,0 +1,11 @@
+module.exports = {
+ transpile: function transpile() {
+  try {
+   return require('./src/program')
+  } catch(e) {
+   throw new (require('./src/errors/TranspileError'))(e.toString())
+ },
+ transpileFile: function transpileFile(file) {
+  return module.exports.transpile(require('fs').readFileSync(file))
+ }
+}
