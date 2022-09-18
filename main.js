0e8064039c37c784ab5a6ecf511e8aab1f6252bc
Dank-Memer-Help <86004860+Dank-Memer-Help@users.noreply.github.com>
2022-09-18 20:33:54 +0700

    Fix missing "}"

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ module.exports = {
    return require('./src/program')
   } catch(e) {
    throw new (require('./src/errors/TranspileError'))(e.toString())
+  }
  },
  transpileFile: function transpileFile(file) {
   return module.exports.transpile(require('fs').readFileSync(file))
