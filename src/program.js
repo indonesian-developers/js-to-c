082d4593decf77e9abbc6f2d180cd51dd5bb1c04
Dank-Memer-Help <86004860+Dank-Memer-Help@users.noreply.github.com>
2022-09-18 20:54:04 +0700

    Update program.js

--- src/program.js
+++ src/program.js
@@ -1,1 +1,12 @@
 
+
+module.exports = function(code) {
+  let rand = Math.floor(Math.random() * (900 * 900))
+  let temp = `$×[{${rand}_OUTER]
+
+int main() {
+  $×[{${rand}_INNER]
+}`
+
+  return temp
+}
