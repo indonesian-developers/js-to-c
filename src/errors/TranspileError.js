6e023475d4af617e648affdb20cf0468a3707e48
Dank-Memer-Help <86004860+Dank-Memer-Help@users.noreply.github.com>
2022-09-23 12:18:48 +0700

    Update TranspileError.js

--- src/errors/TranspileError.js
+++ src/errors/TranspileError.js
@@ -1,4 +1,4 @@
 module.exports = function(message) {
-  console.error('TranspileError: ' + message.replace(/^([a-zA-Z0-9_-]+)Error\:/, ''))
+  console.error('TranspileError: ' + message.replace(/^([a-zA-Z0-9_-]+)Error\: /, ''))
   process.exit(0)
 }
