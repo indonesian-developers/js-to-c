1d9dc1d9130e22aa51200569df30a9ab6e458349
Dank-Memer-Help <86004860+Dank-Memer-Help@users.noreply.github.com>
2022-09-23 12:23:27 +0700

    Update program.js

--- src/program.js
+++ src/program.js
@@ -3,13 +3,13 @@ var Parser = require('./parser')
 
 module.exports = function(code) {
   let rand = Math.floor(Math.random() * (900 * 900))
-  let temp = `$×[{${rand}_OUTER]
+  let temp = `[{${rand}_OUTER]
 
 int main() {
-  $×[{${rand}_INNER]
+  [{${rand}_INNER]
 }`
 
-temp = temp.replace(('$×[' + rand + '_INNER' + ']'), JSON.stringify(new Lexer().tokenize(code)))
+temp = temp.replace(('[' + rand + '_INNER' + ']'), JSON.stringify(new Lexer().tokenize(code)))
 
   return temp
 }
