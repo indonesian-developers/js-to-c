ad6f587a68fa0efc7e2a1a6bd996f2977040252a
Dank-Memer-Help <86004860+Dank-Memer-Help@users.noreply.github.com>
2022-09-23 12:19:52 +0700

    Update program.js

--- src/program.js
+++ src/program.js
@@ -9,7 +9,7 @@ int main() {
   $×[{${rand}_INNER]
 }`
 
-temp = temp.replace((rand + '_INNER'), JSON.stringify(new Lexer.lexer().tokenize(code)))
+temp = temp.replace((rand + '_INNER'), JSON.stringify(new Lexer().tokenize(code)))
 
   return temp
 }
